473797ae50259ad02d6e85eec9252463d2dda12a
namkwa <h.houis1507@gmail.com>
2024-09-12 17:17:43 +0200

    first commit

---
+++ dices.py
@@ -0,0 +1,49 @@
+import json
+
+glyphs_ref = {"N": "Nothing", "C": "Control", "A": "Aim", "R": "Range", "Z": "Zone", "P": "Power"}
+
+
+class Dice:
+    def __init__(self, type, values):
+        self.type = type
+        self.values = values
+        self.name = "default"
+
+    def convert_to_json(self):
+        json_dice = {}
+        json_dice["type"] = self.type
+        json_dice["values"] = self.values
+        json_dice["name"] = self.name
+        return json_dice
+
+
+class Player:
+    def __init__(self):
+        self.dices = []
+        self.name_player = "couille"
+
+    def register(self, glyphs, type):
+        values = {}
+        for i, char in enumerate(glyphs):
+            values[i] = glyphs_ref[char]
+        self.dices.append(Dice(type, values))
+
+    def convert_to_json(self):
+        data_to_store = {}
+        data_to_store["name_player"] = self.name_player
+        data_to_store["dices"] = []
+        for dice in self.dices:
+            data_to_store["dices"].append(dice.convert_to_json())
+        json_object = json.dumps(data_to_store, indent=4)
+        with open("sample1.json", "w") as outfile:
+            outfile.write(json_object)
+
+
+player = Player()
+player.register("NCARZP", "temp")
+player.convert_to_json()
+
+
+class Parser:
+    def __init__(self) -> None:
+        pass
